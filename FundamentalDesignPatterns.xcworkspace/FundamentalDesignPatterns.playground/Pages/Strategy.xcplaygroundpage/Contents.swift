e384d70e5bb3449bb52f55ea8629aaf430046207
Edwin Cardenas <ecardenas@lwolf.com>
2026-02-07 16:57:12 -0500

    feat: strategy pattern

--- FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
@@ -1,13 +1,103 @@
 /*:
  [Previous](@previous)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Next](@next)
- 
+
  # Strategy
  - - - - - - - - - -
  ![Strategy Diagram](Strategy_Diagram.png)
- 
+
  The strategy pattern defines a family of interchangeable objects.
- 
+
  This pattern makes apps more flexible and adaptable to changes at runtime, instead of requiring compile-time changes.
- 
+
  ## Code Example
  */
+import UIKit
+
+public protocol MovieRatingStrategy {
+
+    var ratingSystemName: String { get }
+
+    func fetchRating(
+        for movieTitle: String,
+        success: (_ rating: String, _ review: String) -> Void
+    )
+
+}
+
+public class RottenTomatoesClient: MovieRatingStrategy {
+
+    public let ratingSystemName: String = "Rotten Tomatoes"
+
+    public func fetchRating(
+        for movieTitle: String,
+        success: (_ rating: String, _ review: String) -> Void
+    ) {
+        let rating = "95%"
+        let review = "It rocked!"
+
+        success(rating, review)
+    }
+
+}
+
+public class IMDbClient: MovieRatingStrategy {
+
+    public let ratingSystemName: String = "IMDb"
+
+    public func fetchRating(
+        for movieTitle: String,
+        success: (_ rating: String, _ review: String) -> Void
+    ) {
+        let rating = "3 / 10"
+        let review = """
+            It was terrible! The audience was throwing rotten
+            tomatoes!
+            """
+
+        success(rating, review)
+    }
+
+}
+
+public class MovieRatingController: UIViewController {
+
+    // MARK: - Properties
+
+    public var movieRatingClient: MovieRatingStrategy!
+
+    public let movieTitleTextField = UITextField()
+    public let ratingServiceNameLabel = UILabel()
+    public let ratingLabel = UILabel()
+    public let reviewLabel = UILabel()
+
+    // MARK: - View Lifecycle
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ratingServiceNameLabel.text = movieRatingClient.ratingSystemName
+    }
+
+}
+
+// MARK: - Actions
+
+extension MovieRatingController {
+
+    @objc public func searchButtonTapped(_ sender: UIButton) {
+        guard
+            let movieTitle = movieTitleTextField.text,
+            !movieTitle.isEmpty
+        else { return }
+
+        movieRatingClient.fetchRating(for: movieTitle) {
+            [weak self] rating, review in
+
+            guard let self else { return }
+
+            self.ratingLabel.text = rating
+            self.reviewLabel.text = review
+        }
+    }
+
+}
